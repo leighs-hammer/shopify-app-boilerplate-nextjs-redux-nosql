d96ffe107019aa496e6741bb674a4d631560e734
Leigh Barnes <leighbrenmdonbarnes@gmail.com>
2020-03-24 01:04:05 +0000

    Test install pre cleanup

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Stage from '../components/Stage'
-
+import Axios from 'axios'
+import qs from 'query-string'
 
 const Dashboard = () => {
+
+  const [shop,setShop] = useState({})
+
+  const runInstall = async () => {
+
+    if(typeof window !== 'undefined' && window.location) {
+      const query = qs.parse(window.location.search)
+      Axios.post('/api/install', {
+        shop: query.shop,
+        code: query.code,
+      }).then(data => setShop(data))
+
+    }
+
+  }
 
   return (
     <Stage>
       Dashboard
+      <div onClick={() => runInstall()}>TEST INSTALL</div>
+      {JSON.stringify(shop)}
     </Stage>
   )
 }
